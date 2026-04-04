e86452aa25a174039add41be6f8f9a502867c220
Hai Ning <ninghai@google.com>
2026-04-03 22:34:32 -0400

    Add informative print statement and Apollo 11 test paragraph to ner.py

--- ner.py
+++ ner.py
@@ -8,6 +8,7 @@ def sample_analyze_entities(text_content):
     Args:
       text_content The text content to analyze
     """
+    print(f"Analyzing entities in the following text: '{text_content}'")
 
     client = language_v1.LanguageServiceClient()
 
@@ -65,5 +66,12 @@ def sample_analyze_entities(text_content):
 
 
 if __name__ == "__main__":
-    test_text = "Google is headquartered in Mountain View, California."
+    test_text = (
+        "The Apollo 11 mission was the first spaceflight that landed humans on the Moon. "
+        "Commander Neil Armstrong and lunar module pilot Buzz Aldrin landed the Apollo Lunar Module Eagle "
+        "on July 20, 1969, at 20:17 UTC. Armstrong became the first person to step onto the lunar surface "
+        "six hours and 39 minutes later on July 21; Aldrin joined him 19 minutes later. "
+        "They spent about two and a quarter hours together outside the spacecraft, and they collected "
+        "47.5 pounds of lunar material to bring back to Earth."
+    )
     sample_analyze_entities(test_text)
